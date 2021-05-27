16e86347f088accf0fc9cf76a23c14942bb9e965
Júlio César <juliocholiveira@gmail.com>
2021-05-27 14:49:46 -0300

    feat: novas funcionalidades

--- pages/enlid.js
+++ pages/enlid.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Button } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 import { useState } from "react";
 
@@ -24,25 +25,28 @@ function Enlid() {
 
   return (
     <Container maxWidth="xs">
-      <h3>ENLID VIVAR NEI</h3>
-      <h2>Informe sua matrícula para assinar o compromisso</h2>
+      <Grid item xs={12} style={{ textAlign: "center", paddingTop: 50 }}>
+        <img src="images/ENLID.jpeg" style={{ width: 200 }}></img>
 
-      <TextField
-        id="outlined-basic"
-        label="Matrícula"
-        variant="outlined"
-        helperText="No formato F0000000. Com F e sem dígito."
-        value={chave}
-        onChange={(e) => setChave(e.target.value)}
-      />
+        <h2>Informe sua matrícula para assinar o compromisso</h2>
 
-      <br />
-      <br />
-      <br />
+        <TextField
+          id="outlined-basic"
+          label="Matrícula"
+          variant="outlined"
+          helperText="No formato F0000000. Com F e sem dígito."
+          value={chave}
+          onChange={(e) => setChave(e.target.value)}
+        />
 
-      <Button variant="contained" color="primary" onClick={() => salvar()}>
-        Assinar Compromisso
-      </Button>
+        <br />
+        <br />
+        <br />
+
+        <Button variant="contained" color="primary" onClick={() => salvar()}>
+          Assinar Compromisso
+        </Button>
+      </Grid>
     </Container>
   );
 }
